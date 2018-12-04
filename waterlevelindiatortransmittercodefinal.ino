6cdb4bf3dd448e59c3322bc4fe4cde08671db531
michaelfmc <44358755+michaelfmc@users.noreply.github.com>
2018-12-04 10:34:23 +0200

    Add files via upload

---
+++ waterlevelindiatortransmittercodefinal.ino
@@ -0,0 +1,38 @@
+#include <Ultrasonic.h>
+
+#include <VirtualWire.h>
+
+
+const int trigPin = 8; //where the oin is connected on the arduino
+const int echoPin = 9;
+
+
+float temp; //Define the distance float variable
+char msg[6];
+
+
+void setup()
+{
+  
+  vw_setup(2000);	        // Bits per sec
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+ digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+temp = pulseIn(echoPin, HIGH);//assigning the digital output to temp
+
+    temp = temp * 0.034/2; //converting volts to degrees celsius ----- 0.48828125 = [(5V*1000)/1024]10
+    dtostrf(temp, 6,2,msg);           //converts the float into a char 
+    vw_send((uint8_t *)msg, strlen(msg)); //transmits the data
+    vw_wait_tx(); // Wait until the whole message is gone
+    delay(200);
+ }
+ 
---
+++ waterlevelindicatorreceivercodefinal.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+#include <LiquidCrystal.h>
+
+#include <VirtualWire.h>
+
+
+LiquidCrystal lcd(6, 7, 8, 9, 10, 11);//Initialise the LCD
+
+
+int i;    //and integer used to count
+
+void setup()
+{
+    //Define the LCD as 16 column by 2 rows 
+    lcd.begin (16,2);
+   //Define the receiver pin and rate
+    vw_set_rx_pin(12);       //Sets pin D12 as the RX Pin
+    vw_setup(2000);	     // Bits per sec
+    vw_rx_start();           // Start the receiver PLL running
+}
+
+void loop()
+{
+   uint8_t buf[VW_MAX_MESSAGE_LEN];
+   uint8_t buflen = VW_MAX_MESSAGE_LEN;
+   
+   if( vw_get_message(buf, &buflen) )
+      {
+        lcd.setCursor(0, 0);
+        lcd.print("The Distance is:");    
+        lcd.setCursor(0,1);  
+        
+       for (i = 0; i < buflen; i++)
+         { 
+           lcd.write(buf[i]); 
+                    
+         }
+        
+           //lcd.print((char)223);
+           lcd.print("Cm");
+
+      }
+ 
+}
